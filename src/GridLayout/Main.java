93502cd832351daeae555e5d029b45df29945f59
tekerinkin <dmshestakov@icloud.com>
2022-05-10 16:55:03 +0300

    Add GridLayout

---
+++ src/GridLayout/Main.java
@@ -0,0 +1,25 @@
+package GridLayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new GridLayout(3,3,30,30));
+
+        frame.add(new Button("1"));
+        frame.add(new Button("2"));
+        frame.add(new Button("3"));
+        frame.add(new Button("4"));
+        frame.add(new Button("5"));
+        frame.add(new Button("6"));
+        frame.add(new Button("7"));
+        frame.add(new Button("8"));
+        frame.add(new Button("9"));
+
+        frame.setVisible(true);
+    }
+}
